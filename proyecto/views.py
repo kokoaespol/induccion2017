6c85df93a571c2778eb23e4893dab39f2bd94ad7
ngalvare <ngalvare@hotmail.com>
2017-04-05 12:01:42 -0500

    [FIX]

--- proyecto/views.py
+++ proyecto/views.py
@@ -100,31 +100,31 @@ def acertijo_list(request):
 
 def savePost(request):
     user_form = UserForm()
-	profile_form = ProfileForm()
-	acertijo_form = AcertijoForm()
-	facultad_form = FacultadForm()
-	medalla_form = MedallaForm()
-	tesoro_form = TesoroForm()
+    profile_form = ProfileForm()
+    acertijo_form = AcertijoForm()
+    facultad_form = FacultadForm()
+    medalla_form = MedallaForm()
+    tesoro_form = TesoroForm()
     opcion_form = OpcionForm()
-	if request.method == 'POST':
-		user_form = UserForm(request.POST,prefix="user")
-		profile_form = ProfileForm(request.POST, prefix = "profile")
+    if request.method == 'POST':
+        user_form = UserForm(request.POST,prefix="user")
+        profile_form = ProfileForm(request.POST, prefix = "profile")
         acertijo_form = AcertijoForm(request.POST, prefix = "acertijo")
         facultad_form = FacultadForm(request.POST, prefix = "facultad")
         medalla_form = MedallaForm(request.POST, prefix = "medalla")
         tesoro_form = TesoroForm(request.POST, prefix = "tesoro")
         opcion_form = OpcionForm(request.POST, prefix = "opcion")
         if profile_form.is_valid() and acertijo_form.is_valid() and facultad_form.is_valid() and medalla_form.is_valid() and tesoro_form.is_valid() and opcion_form.is_valid():
-        	profile_form.save()
-        	acertijo_form.save()
-        	facultad_form.save()
-        	medalla_form.cleaned_data["medalla"]
-        	medalla_form.save()
-        	tesoro_form.cleaned_data["tesoro"]
-        	tesoro_form.save()
+            profile_form.save()
+            acertijo_form.save()
+            facultad_form.save()
+            medalla_form.cleaned_data["medalla"]
+            medalla_form.save()
+            tesoro_form.cleaned_data["tesoro"]
+            tesoro_form.save()
             opcion_form.save()
         else:
-        	return HttpResponse(status=500)
-		return HttpResponse(status=200)
-	else:
-		return HttpResponse(status=400)
+            return HttpResponse(status=500)
+        return HttpResponse(status=200)
+    else:
+        return HttpResponse(status=400)
