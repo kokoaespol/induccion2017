c53a03d0f5f66701ebf201ef47643e9600909104
maxamend <marcoxavibsc@gmail.com>
2017-04-12 11:32:58 -0500

    [FIX] archivo serializers, [ADD] campo de id a todos los serializers

--- proyecto/serializers.py
+++ proyecto/serializers.py
@@ -5,15 +5,14 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
-            'first_name','last_name','username',
+            'id','first_name','last_name','username',
         )
 
 class ProfileSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
     class Meta:
         model = Profile
         fields = (
-            'user','color_piel','color_camiseta',
+            'id','user','color_piel','color_camiseta',
             'color_cabello','sexo','ranking',
         )
 
@@ -21,41 +20,35 @@ class AcertijoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Acertijo
         fields = (
-            'titulo','descripcion','respuesta','respondido',
+            'id','titulo','descripcion','respuesta','respondido',
         )
 
 class FacultadSerializer(serializers.ModelSerializer):
-    profile = ProfileSerializer()
     class Meta:
         model = Facultad
         fields = (
-            'nombre','profile',
+            'id','nombre','profile',
         )
 
 class MedallaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Medalla
         fields = (
-            'titulo','descripcion','n_tesoro',
+            'id','titulo','descripcion','n_tesoro',
             'icono',
         )
 
 class TesoroSerializer(serializers.ModelSerializer):
-    perfil = ProfileSerializer()
-    medalla = MedallaSerializer()
-    facultad = FacultadSerializer()
-    acertijo = AcertijoSerializer()
     class Meta:
         model = Tesoro
         fields = (
-            'nombre','descripcion','icono',
+            'id','nombre','descripcion','icono',
             'perfil','medalla','facultad','acertijo',
         )
 
 class OpcionSerializer(serializers.ModelSerializer):
-    acertijo = AcertijoSerializer()
     class Meta:
         model =  Opcion
         fields = (
-            'acertijo','texto','correcto',
+            'id','acertijo','texto','correcto',
         )
